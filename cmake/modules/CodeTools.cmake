e527ccc1a0f0b2c1f272d16c1ea645d4f1dc14e8
amaa-99 <amaa-99>
2026-06-01 09:49:14 +0000

    Enable additional compiler warnings: Added the MVVM_ENABLE_EXTRA_COMPILER_WARNINGS cmake flag for enabling additional compiler warnings. Enabled more compiler warnings in case MVVM_ENABLE_EXTRA_COMPILER_WARNINGS is enabled.

--- cmake/modules/CodeTools.cmake
+++ cmake/modules/CodeTools.cmake
@@ -46,17 +46,15 @@ function(project_testcoverage_setup)
         )
 endfunction()
 
-# Fetches google test version 1.12.1
+# Fetches google test.
 function(fetch_googletest)
   include(FetchContent)
   FetchContent_Declare(googletest GIT_REPOSITORY https://github.com/google/googletest.git GIT_TAG v1.17.0)
-
-FetchContent_MakeAvailable(googletest)
+  FetchContent_MakeAvailable(googletest)
   message(VERBOSE "GTest binaries are present at ${googletest_BINARY_DIR}")
 endfunction()
 
-# Setups targets for code processing.
-
+# Sets up targets for code processing.
 function(project_codetools_setup)
     if (MVVM_SETUP_CLANGFORMAT)
         project_clangformat_setup()
